01a966a37378e74c712c882089200cb42ad60139
cryptowrath <pawelzelenoff@yandex.ru>
2021-03-08 00:29:46 +0300

    Add main file

---
+++ tetris.py
@@ -0,0 +1,39 @@
+import pygame as pg
+from pygame.locals import *
+import sys
+
+FPS = 60
+WHITE = (255, 255, 255)
+GREEN = (0, 200, 64)
+WIN_WEIGHT = 400
+WIN_HEIGHT = 400
+
+
+
+def main():
+    global root, x, y, a, b
+    a = 30
+    b = 30
+    x = 0 
+    y = WIN_HEIGHT // 2
+    pg.init()
+    root = pg.display.set_mode((WIN_WEIGHT, WIN_HEIGHT))
+    pg.display.set_caption('Cryptowrath')
+    pg.display.update()
+    while 1:
+            
+        for i in pg.event.get():
+            if i.type == QUIT:
+                pg.quit()
+                sys.exit()
+        pg.display.update()
+        root.fill(WHITE)
+        pg.draw.rect(root, GREEN, (x, y, a, b))
+        pg.display.update()
+    
+        x += 2
+        
+        pg.time.delay(50)
+main()
+draw()
+

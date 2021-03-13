254f49f16869503d06aafe7516e4a5a6a5e227ef
cryptowrath <pawelzelenoff@yandex.ru>
2021-03-13 15:07:24 +0300

    get a little bit better

--- tetris.py
+++ tetris.py
@@ -11,28 +11,19 @@ WEIGHT = 400
 HEIGHT = 800
 x = 30
 y = 30
+
 coordinates = [[x, y], [x + 30, y],
               [x + 30, y - 30] , [x + 60, y - 30],
               [x + 60, y] ,[x + 90, y],
-              [x + 90, y + 30],[x, y + 30]]
+              [x + 90, y + 30],[30, y + 30]]
 
-coordinates_x = [coordinates[0][0],
-                coordinates[1][0],
-                coordinates[2][0],
-                coordinates[3][0],
-                coordinates[4][0],
-                coordinates[5][0],
-                coordinates[6][0],
-                coordinates[7][0],]
 
-coordinates_y = [coordinates[0][1],
-                coordinates[1][1],
-                coordinates[2][1],
-                coordinates[3][1],
-                coordinates[4][1],
-                coordinates[5][1],
-                coordinates[6][1],
-                coordinates[7][1],]
+class Properties:
+    def __init__(self, coordinates, color, widget):
+        self.coordinates = []
+        self.color = ""
+        self.widget = ""
+
 
 def main():
     global root, x, y, a, b
@@ -55,33 +46,29 @@ def main():
         pg.draw.polygon(root, GREEN, coordinates)
 
         if keys[K_LEFT]:
-            x -= 20
+            for i in coordinates:
+                i[0] -= 20
         elif keys[K_RIGHT]:
-            for i in coordinates_x:
-                i += 20
+            for i in coordinates:
+                i[0] += 20
         elif keys[K_UP]:
-            for i in coordinates_y:
-                i -= 20
+            for i in coordinates:
+                i[1] -= 20
         elif keys[K_DOWN]:
-            for i in coordinates_y:
-                i += 20
+            for i in coordinates:
+                i[1] += 20
         else:
-            for i in coordinates_y:
-                i += 20
+            for i in coordinates:
+                i[1] += 20
         if keys[K_DOWN] and keys[K_LEFT]:
-            x -= 20
-            y += 20
+            for i in coordinates:
+                i[1] += 20
+                i[0] -= 20
         if keys[K_DOWN] and keys[K_RIGHT]:
-            y += 20
-            x += 20
-        if y >= HEIGHT + b:
-            y = 0 - b
-        elif x >= WEIGHT + a:
-            x = 0 - a
-        elif x <= 0 - a:
-            x = WEIGHT + a
-        elif y <= 0 - b:
-            y = HEIGHT + b
+            for i in coordinates:
+                i[1] += 20
+                i[0] += 20
+
         pg.display.update()
 
         pg.time.delay(50)

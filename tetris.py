b795133475d190c63ba54b8e25213e23d8082e4b
cryptowrath <pawelzelenoff@yandex.ru>
2021-03-15 00:11:49 +0300

    now tetramino move limited of size screen

--- tetris.py
+++ tetris.py
@@ -9,16 +9,18 @@ GREEN = (0, 200, 64)
 BLUE = (0, 0, 255)
 WEIGHT = 400
 HEIGHT = 800
-x = 30
-y = 30
+COLORS = [WHITE, GREEN, BLUE]
+x = 40
+y = 0
+offset = 20
 
 coordinates = [[x, y], [x + 30, y],
               [x + 30, y - 30] , [x + 60, y - 30],
               [x + 60, y] ,[x + 90, y],
-              [x + 90, y + 30],[30, y + 30]]
+              [x + 90, y + 30],[x, y + 30]]
 
 
-class Properties:
+class Tetramino:
     def __init__(self, coordinates, color, widget):
         self.coordinates = []
         self.color = ""
@@ -26,15 +28,15 @@ class Properties:
 
 
 def main():
-    global root, x, y, a, b
-    a = 100
-    b = 50
-    x = WEIGHT // 2
-    y = 0 - b
+    global root
+
     pg.init()
+
     root = pg.display.set_mode((WEIGHT, HEIGHT))
+    screen_rect = root.get_rect()
     pg.display.set_caption('Tetris')
     pg.display.update()
+
     while 1:
         for i in pg.event.get():
             if i.type == QUIT:
@@ -43,33 +45,38 @@ def main():
         pg.time.delay(10)
         keys = pg.key.get_pressed()
         root.fill(BLUE)
-        pg.draw.polygon(root, GREEN, coordinates)
+        tetramino = pg.draw.polygon(root, COLORS[1], coordinates)
 
         if keys[K_LEFT]:
             for i in coordinates:
-                i[0] -= 20
+                i[0] -= offset
         elif keys[K_RIGHT]:
             for i in coordinates:
-                i[0] += 20
+                i[0] += offset
         elif keys[K_UP]:
             for i in coordinates:
-                i[1] -= 20
+                i[1] -= offset
         elif keys[K_DOWN]:
             for i in coordinates:
-                i[1] += 20
+                i[1] += offset
         else:
             for i in coordinates:
-                i[1] += 20
+                i[1] += offset
         if keys[K_DOWN] and keys[K_LEFT]:
             for i in coordinates:
-                i[1] += 20
-                i[0] -= 20
+                i[1] += offset
+                i[0] -= offset
         if keys[K_DOWN] and keys[K_RIGHT]:
             for i in coordinates:
-                i[1] += 20
-                i[0] += 20
-
-        pg.display.update()
+                i[1] += offset
+                i[0] += offset
+        if coordinates[0][0] <= 0 - offset and coordinates[7][0] <= 0 - offset:
+            for i in coordinates:
+                i[0] += offset
+        elif coordinates[6][1] >= HEIGHT + 15 and coordinates[7][1] >= HEIGHT + 15:
+            for i in coordinates:
+                i[1] -= offset
+        pg.display.flip()
 
         pg.time.delay(50)
 main()
